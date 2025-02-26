fcae97c0bbf306e9b737122ae7f9b22191f29357
Khoa Nguyen <colormak3r@gmail.com>
2025-02-25 22:37:07 -0800

    Set up basic home and about pages

---
+++ app/routes/home.tsx
@@ -0,0 +1,62 @@
+import NavBar from "../components/navbar";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <div className="h-screen bg-gradient-to-b from-red-300 to-orange-200">
+        <h1 className="text-3xl p-4">Welcome to Nom Nom</h1>
+        <p className="p-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          blandit scelerisque ex, at pharetra lacus tempus sed. Maecenas
+          vestibulum lobortis tincidunt. Pellentesque posuere facilisis nulla,
+          non interdum velit molestie eget. Duis pellentesque elit eu purus
+          rhoncus, vel consectetur arcu gravida. Etiam eleifend ipsum nulla, vel
+          volutpat tellus venenatis at. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Proin imperdiet mi dapibus sem fermentum consectetur.
+          Integer suscipit enim id ipsum pulvinar, vitae facilisis ex vehicula.
+          Donec non massa viverra, ullamcorper velit vestibulum, maximus nulla.
+          Vestibulum nec elit ornare libero auctor euismod in eget arcu. In
+          commodo tellus id vestibulum laoreet. Maecenas vel rhoncus justo.
+          Morbi pretium est feugiat tellus dapibus elementum. Cras varius
+          molestie urna non sollicitudin. Proin eleifend varius ipsum ut
+          interdum. Maecenas pellentesque velit id finibus pharetra.
+          Pellentesque placerat libero pellentesque lectus consequat volutpat.
+          Praesent mollis sollicitudin maximus. Sed vehicula nunc ligula, vitae
+          ultrices justo ultrices sed. Integer blandit porta lorem eu interdum.
+          Vestibulum dolor magna, egestas eget scelerisque sit amet, maximus id
+          augue. Fusce dignissim sapien mauris, vitae maximus risus accumsan eu.
+          Vivamus tincidunt tellus vitae tristique ultrices. Donec id nisi
+          mollis, ullamcorper orci id, ultrices eros. Aenean ullamcorper mattis
+          nulla, id pulvinar nisl placerat sit amet. Morbi lobortis mi quis
+          ultricies mattis. Nam justo quam, tempus nec interdum ac, bibendum et
+          nibh. Etiam at lectus commodo purus semper mollis id in massa.
+          Pellentesque molestie rhoncus risus et rhoncus. Maecenas tempor nunc a
+          lorem volutpat, nec malesuada purus maximus. Fusce molestie, eros ut
+          facilisis consectetur, erat mi efficitur ipsum, quis rutrum lectus
+          lacus eu augue. Maecenas luctus libero quis nisl interdum, sit amet
+          laoreet odio dignissim. Donec eu venenatis sapien. Nam ac ex
+          sollicitudin, cursus nunc nec, tristique lorem. Nunc feugiat eros sit
+          amet tincidunt tincidunt. Praesent quis gravida quam. Nullam odio
+          tortor, consectetur in efficitur sed, pharetra at sapien. Quisque ante
+          nulla, vestibulum id justo at, maximus efficitur lorem. Proin vitae
+          ipsum lacinia, ultrices leo quis, pulvinar enim. Nullam dapibus
+          ullamcorper leo fermentum viverra. Duis fringilla mi at turpis
+          pellentesque ultricies. Pellentesque rhoncus sed purus vitae
+          malesuada. Mauris volutpat quis neque non sodales. Nulla et commodo
+          dolor. Praesent at massa in nibh fringilla dapibus ac vitae mi.
+          Integer ante leo, luctus eu facilisis non, ornare ac justo. Aliquam
+          sit amet justo in metus tristique ultrices eget sit amet felis. Nulla
+          suscipit aliquet volutpat. Suspendisse potenti. Vestibulum vel felis
+          nec nisi finibus tincidunt. Suspendisse tempus sem mauris, ut pulvinar
+          orci sollicitudin vitae. Pellentesque quis iaculis ligula, et sagittis
+          neque. Cras sed diam sed nibh interdum eleifend eu eu tortor.
+          Curabitur auctor, ante vitae pellentesque convallis, tortor risus
+          bibendum elit, a molestie arcu ex vel nisi. Quisque ultrices interdum
+          lacus volutpat volutpat. Nullam et dui id sapien fringilla placerat
+          eget nec tortor. Etiam vel leo elit.
+        </p>
+      </div>
+    </>
+  );
+}
