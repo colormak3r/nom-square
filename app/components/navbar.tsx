3faff9ae57fc7270c57dcf5c54c9300c0006bd2b
tAmh <ttrhuynh07@gmail.com>
2025-02-28 22:26:23 -0800

    setup database

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,15 +1,20 @@
 export default function Navbar() {
-    return (
-        <nav className="flex items-center justify-between p-4 bg-red-400 sticky top-0 shadow-md z-50">
-            <div className="text-xl font-bold text-stone-700">Nom<sup>2</sup></div>
-            <div className="space-x-4">
-                <a href="/" className="text-stone-700 hover:text-stone-900">
-                    Home
-                </a>
-                <a href="/about" className="text-stone-700 hover:text-stone-900">
-                    About
-                </a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="flex items-center justify-between p-4 bg-red-400 sticky top-0 shadow-md z-50">
+      <div className="text-xl font-bold text-stone-700">
+        Nom<sup>2</sup>
+      </div>
+      <div className="space-x-4">
+        <a href="/" className="text-stone-700 hover:text-stone-900">
+          Home
+        </a>
+        <a href="/about" className="text-stone-700 hover:text-stone-900">
+          About
+        </a>
+        <a href="/employee" className="text-stone-700 hover:text-stone-900">
+          Employee
+        </a>
+      </div>
+    </nav>
+  );
 }
