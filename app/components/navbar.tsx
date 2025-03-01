6830d14894003fababa59f5dada6bb201528ace4
Khoa Nguyen <colormak3r@gmail.com>
2025-03-01 00:07:01 -0800

    Added category and menu cards

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,12 +1,13 @@
 export default function Navbar() {
     return (
         <nav className="flex items-center justify-between p-4 bg-red-400 sticky top-0 shadow-md z-50">
-            <div className="text-xl font-bold text-stone-700">Nom<sup>2</sup></div>
-            <div className="space-x-4">
-                <a href="/" className="text-stone-700 hover:text-stone-900">
+            <div className="text-xl font-bold text-stone-700 border rounded-2xl p-4">Nom<sup>2</sup></div>
+            <div className="flex flex-row space-x-4">
+                <a href="/" className="text-stone-700 font-semibold hover:text-stone-900">
                     Home
                 </a>
-                <a href="/about" className="text-stone-700 hover:text-stone-900">
+                <p className="text-stone-700 font-semibold">•</p>
+                <a href="/about" className="text-stone-700 font-semibold hover:text-stone-900">
                     About Us
                 </a>
             </div>
